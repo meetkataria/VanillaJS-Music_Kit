8649aeb3cf3f3f763e45602358f7b923cd4f4683
meetkataria <61610828+meetkataria@users.noreply.github.com>
2021-04-17 11:44:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('keydown', function(e){
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(audio) audio.play();
     let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    audio.currentTime = 0;
     key.classList.add('playing');
     // key.classList.remove('playing');           
 });
